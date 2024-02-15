4774c8a570ee51962da618a82a764ad06d31f0a0
Aftab Hussain <aftabdars@gmail.com>
2024-02-16 01:54:40 +0500

    socket client setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import Nav from './components/Nav';
 import MainContent from './components/MainContent';
 import './components/style/App.css';
+import { socket } from './socket';
+import { useEffect, useState } from 'react';
 
 export default function App () {
+    const [isConnected, setIsConnected] = useState(socket.connected)
+    useEffect(()=>{
+    function onConnect(){
+      setIsConnected(true)
+    }
+    function onDisconnect(){
+      setIsConnected(false)
+    }
+    socket.on('connect', onConnect)
+    socket.on('disconnect', onDisconnect)
+  })
     return (
         <div className="main-container center">
         <Nav />
